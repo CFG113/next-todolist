1aface5ce809ba589d53f8752dc9a0c3e724e6de
Chr1stian234 <s3892544@student.rmit.edu.au>
2024-12-15 14:48:43 +1100

    my todolist

---
+++ src/components/TodoTask.tsx
@@ -0,0 +1,20 @@
+'use client';
+
+export type Task = {
+    id: number;
+    task: string;
+    is_complete: boolean;
+}
+  
+  export default function TodoTask({
+    task,
+  }: {
+    task: Task;
+  }) {
+    return (
+      <div>
+        <h3>{task.task}</h3>
+        <p>Completed: {task.is_complete ? 'Yes' : 'No'}</p>
+      </div>
+    )
+  }
---
+++ src/hooks/useTodos.ts
@@ -0,0 +1,54 @@
+import { supabase } from "@/lib/supabase";
+
+export const useTodos = () => {
+
+    const addTodo = async(taskName: string) => {
+        const { data, error } = await supabase
+            .from('tasks')
+            .insert([{ task: taskName }])
+            .select()
+            .single()
+
+        if (error) { throw new Error(`Failed to add task: ${error.message}`) }
+
+        return data;
+    }
+
+    const getTodos = async () => {
+        const { data, error } = await supabase.from('tasks').select('*')
+        
+        if (error) { throw new Error(`Failed to get tasks: ${error.message}`) }
+        
+        return data;
+        
+    }
+
+    const updateTodo = async (taskId: number, updatedTaskName: string) => {
+        const { data, error } = await supabase
+            .from('tasks')
+            .update({ task: updatedTaskName })
+            .eq('id', taskId)
+            .select()
+            .single()
+
+        if (error) { throw new Error(`Failed to update task: ${error.message}`) }
+
+        return data; 
+    }
+
+    const deleteTodo = async (taskId: number) => {
+        const { data, error } =  await supabase
+            .from('tasks')
+            .delete()
+            .eq('id',taskId)
+            .select()
+            .single()
+
+        if (error ) { throw new Error(`Failed to delete task: ${error.message}`) } 
+
+        return data;
+    }
+    
+    return { addTodo, getTodos, updateTodo, deleteTodo }
+}
+
