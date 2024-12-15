51fc2ddb1cc09d95c5fdac99e5916f4f948ff1b7
Chr1stian234 <s3892544@student.rmit.edu.au>
2024-12-15 19:44:02 +1100

    Revert "my todolist"
    This reverts commit 1aface5ce809ba589d53f8752dc9a0c3e724e6de.

--- src/components/TodoTask.tsx
+++ src/components/TodoTask.tsx
@@ -1,38 +1,38 @@
 'use client';
 
 export type Task = {
-    id: number;
-    task: string;
-    is_complete: boolean;
+  id: number;
+  task: string;
+  is_complete: boolean;
+};
+
+type TodoTaskProps = {
+  task: Task;
+  updateTodo: (taskId: number, updatedTaskName: string) => void;
+  deleteTodo: (taskId: number) => void;
+};
+
+export default function TodoTask({ task, updateTodo, deleteTodo }: TodoTaskProps) {
+  return (
+    <li className="flex items-center mb-4 space-x-4">
+      <div className="flex-grow">{task.task}</div>
+      <button
+        onClick={() => {
+          const updatedName = prompt('Update Task', task.task);
+          if (updatedName) {
+            updateTodo(task.id, updatedName);
+          }
+        }}
+        className="bg-blue-500 text-white px-4 py-2 rounded"
+      >
+        Update
+      </button>
+      <button
+        onClick={() => deleteTodo(task.id)}
+        className="bg-red-500 text-white px-4 py-2 rounded"
+      >
+        Delete
+      </button>
+    </li>
+  );
 }
-  
-  export default function TodoTask({
-    task,
-  }: {
-    task: Task;
-  }) {
-    return (
-      <div>
-        <li key={task.id} className="flex items-center mb-4 space-x-4">
-            {/* Display task */}
-            <span className="flex-grow">{task.task}</span>
-
-            {/* Update Button */}
-            <button
-
-                className="bg-blue-500 text-white px-4 py-2 rounded"
-            >
-                Update
-            </button>
-
-            {/* Delete Button */}
-            <button
-
-                className="bg-red-500 text-white px-4 py-2 rounded"
-            >
-                Delete
-            </button>
-        </li>
-      </div>
-    )
-  }
