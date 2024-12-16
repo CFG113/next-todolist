9a0d91240ff081d7f9eb9dc72defe368e0c3e866
Chr1stian234 <s3892544@student.rmit.edu.au>
2024-12-16 19:47:03 +1100

    hook error handling i still need to display it on the front end

--- src/hooks/useTodos.ts
+++ src/hooks/useTodos.ts
@@ -1,64 +1,89 @@
 
 
 import { supabase } from "@/lib/supabase";
-import { useEffect, useState } from "react";
+import { useEffect, useState } from "react"
 import { Task } from '@/components/TodoTask'
+import { PostgrestError } from "@supabase/supabase-js";
 
 export const useTodos = () => {
     const [todos, setTodos] = useState<Task[]>([])
     const [task, setTask] = useState('')
     const [loading, setLoading] = useState(false)
 
+
     useEffect(() => {
         const getTodos = async () => {
             setLoading(true)
-            const { data } = await supabase.from('tasks').select('*').throwOnError()
-            
-            setTodos(data || [])
-            setLoading(false)
+            try {
+                const { data } = await supabase.from('tasks').select('*').throwOnError()
+                setTodos(data || [])
+            } catch(error) {
+                const postgrestError = error as PostgrestError
+                console.error('Failed to get tasks:', postgrestError.message)
+            } finally {
+                setLoading(false)
+            }
         }
 
         getTodos()
     }, [])
+
     const addTodo = async(taskName: string) => {
         setLoading(true)
-        const { data } = await supabase
-            .from('tasks')
-            .insert([{ task: taskName }])
-            .select()
-            .single()
-            .throwOnError()
+        try {
+            const { data } = await supabase
+                .from('tasks')
+                .insert([{ task: taskName }])
+                .select()
+                .single()
+                .throwOnError()
 
-        
-        setTodos((prev) => [...prev, data])
-        setTask('')
-        setLoading(false)
+            setTodos((prev) => [...prev, data])
+            setTask('')
+        } catch (error) {
+            const postgrestError = error as PostgrestError
+            console.error('Failed to add tasks:', postgrestError.message)
+        } finally {
+            setLoading(false)
+        }
     }
 
     const updateTodo = async (taskId: number, updatedTaskName: string) => {
         setLoading(true)
-        const { data } = await supabase
-            .from('tasks')
-            .update({ task: updatedTaskName })
-            .eq('id', taskId)
-            .select()
-            .single()
-            .throwOnError()
+        try {
+            const { data } = await supabase
+                .from('tasks')
+                .update({ task: updatedTaskName })
+                .eq('id', taskId)
+                .select()
+                .single()
+                .throwOnError()
 
-        setTodos((prev) => prev.map((task) => (task.id === taskId ? data : task)))
-        setLoading(false)
+            setTodos((prev) => prev.map((task) => (task.id === taskId ? data : task)))
+        } catch(error) {
+            const postgrestError = error as PostgrestError
+            console.error('Failed to update tasks:', postgrestError.message)
+        } finally {
+            setLoading(false)
+        }
     }
 
     const deleteTodo = async (taskId: number) => {
         setLoading(true)
-        await supabase
-            .from('tasks')
-            .delete()
-            .eq('id',taskId)
-            .throwOnError()
+        try {
+            await supabase
+                .from('tasks')
+                .delete()
+                .eq('id',taskId)
+                .throwOnError()
             
-        setTodos((prev) => prev.filter((task) => task.id !== taskId))
-        setLoading(false)
+            setTodos((prev) => prev.filter((task) => task.id !== taskId))
+        } catch (error) {
+            const postgrestError = error as PostgrestError
+            console.error('Failed to update tasks:', postgrestError.message)
+        } finally {
+            setLoading(false)
+        }
     }
     
     return { todos, task, setTask, loading, addTodo, updateTodo, deleteTodo };
